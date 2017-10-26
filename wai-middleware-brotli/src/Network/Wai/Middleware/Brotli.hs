fb3631d0e03ebefaa84b862cd1ac743e46d3219f
Ian Duncan <ian@iankduncan.com>
2017-10-26 10:33:53 +0900

    Improving wai middleware

--- wai-middleware-brotli/src/Network/Wai/Middleware/Brotli.hs
+++ wai-middleware-brotli/src/Network/Wai/Middleware/Brotli.hs
@@ -1,15 +1,88 @@
 {-# LANGUAGE OverloadedStrings #-}
 module Network.Wai.Middleware.Brotli
-  (brotli
+  ( brotli
+  , brotli'
+  , defaultSettings
+  , BrotliSettings(..)
+  , defaultShouldCompress
+  , BrotliFiles(..)
   ) where
-import Data.Binary.Builder (toLazyByteString, fromLazyByteString)
-import qualified Data.ByteString.Char8 as B
+
 import Codec.Compression.Brotli
+import Control.Monad
+import Data.Binary.Builder (toLazyByteString, fromLazyByteString, fromByteString, Builder)
+import qualified Data.ByteString.Char8 as B
+import qualified Data.ByteString.Lazy as L
 import Data.IORef
+import Data.Maybe
 import Data.Monoid
+import qualified Data.Trie as Trie
 import Network.HTTP.Types.Header
 import Network.Wai
 import Network.Wai.Internal
+
+data BrotliFiles
+  = BrotliIgnore
+  | BrotliCompress
+  | BrotliCacheFolder FilePath
+  | BrotliPreCompressed BrotliFiles
+  deriving (Read, Eq, Show)
+
+data BrotliSettings = BrotliSettings
+  { brotliCompressionSettings :: !CompressionSettings
+  , brotliMinimumSize :: Int
+  , brotliFilesBehavior :: BrotliFiles
+  , brotliShouldCompress :: BrotliSettings -> Request -> Response -> Bool
+  , brotliMimePrefixes :: [B.ByteString]
+  , brotliMimeSuffixes :: [B.ByteString]
+  }
+
+defaultShouldCompress :: BrotliSettings -> Request -> Response -> Bool
+defaultShouldCompress settings req resp =
+  let (mclb, hs) = pluckHeader hContentLength (responseHeaders resp)
+      (mctb, hs') = pluckHeader hContentType hs
+  in case mctb of
+       Nothing -> False
+       Just ctb ->
+         case mclb of
+           Nothing -> checkMime settings ctb
+           Just clb ->
+             (fromMaybe True $ do
+                (x, rest) <- B.readInt clb
+                return $
+                  if x >= brotliMinimumSize settings
+                    then True
+                    else False) &&
+             checkMime settings ctb
+
+checkMime :: BrotliSettings -> B.ByteString -> Bool
+checkMime settings ctb =
+  any (\b -> b `B.isPrefixOf` ctb) (brotliMimePrefixes settings) ||
+  any (\b -> b `B.isSuffixOf` ctb) (brotliMimeSuffixes settings)
+
+defaultSettings :: BrotliSettings
+defaultSettings = BrotliSettings defaultCompressionSettings 860 BrotliIgnore defaultShouldCompress
+  [ "text/"
+  , "application/json"
+  , "application/javascript"
+  , "application/x-javascript"
+  , "application/ecmascript"
+  , "application/xml"
+  , "application/x-font-ttf"
+  , "image/x-icon"
+  , "image/vnd.microsoft.icon"
+  , "application/vnd.ms-fontobject"
+  , "application/x-font-opentype"
+  , "application/x-font-truetype"
+  , "font/eot"
+  , "font/otf"
+  , "font/ttf"
+  , "font/opentype"
+  ]
+  [ "+json"
+  , "+xml"
+  ]
+
 
 pluckHeader :: HeaderName -> [Header] -> (Maybe B.ByteString, [Header])
 pluckHeader hName = foldr go (Nothing, [])
@@ -43,9 +116,12 @@ decodeRequestBody req = do
       in case encs of
            ("br":encs) -> do
              bodyDecompressor <- decomp getBs
-             return $ req { requestHeaders = ("Content-Encoding", B.intercalate ", " encs) : hs
-                          , requestBody = bodyDecompressor
-                          }
+             return $
+               req
+               { requestHeaders =
+                   ("Content-Encoding", B.intercalate ", " encs) : hs
+               , requestBody = bodyDecompressor
+               }
            _ -> return req
   where
     decomp :: IO B.ByteString -> IO (IO B.ByteString)
@@ -70,7 +146,11 @@ decodeRequestBody req = do
             Error -> error "Brotli stream decoding error in request body"
 
 brotli :: Middleware
-brotli app req sendResponse = do
+brotli = brotli' defaultSettings
+
+brotli' :: BrotliSettings -> Middleware
+brotli' settings app req sendResponse = do
+  let shouldCompress = (brotliShouldCompress settings) settings req
   decodedReq <-
     decodeRequestBody $
     (reqWithoutBrotliAcceptEnc
@@ -81,18 +161,36 @@ brotli app req sendResponse = do
   -- TODO support caching files
   app decodedReq $ \res ->
     case res of
-      ResponseFile {} -> sendResponse res
+      ResponseFile status hs fp bp -> case brotliFilesBehavior settings of
+        BrotliIgnore -> sendResponse res
       (ResponseBuilder status hs b) ->
-        if isBrotliResp
+        if isBrotliResp && shouldCompress res
           then do
-            let compressedResp = compress $ toLazyByteString b
+            let compressedResp =
+                  compressWith
+                    (toLazyByteString b)
+                    (brotliCompressionSettings settings)
             sendResponse $
-              responseLBS status (addBrotliToContentEnc hs) compressedResp
+              responseLBS
+                status
+                (addBrotliToContentEnc $ fixHeaders hs)
+                compressedResp
           else sendResponse res
       (ResponseStream status hs f) ->
-        sendResponse $
-        ResponseStream status (addBrotliToContentEnc hs) $ \send flush ->
-          f (send . fromLazyByteString . compress . toLazyByteString) flush
+        if isBrotliResp && shouldCompress res
+        then sendResponse $
+          ResponseStream status (addBrotliToContentEnc $ fixHeaders hs) $ \send flush -> do
+            c <- compressor (brotliCompressionSettings settings)
+            ref <- newIORef (c, False)
+            f (compressSend send flush ref) (compressFlush ref)
+            (c', shouldFlush) <- readIORef ref
+            when shouldFlush flush
+            case c' of
+              Consume consumer -> consumer B.empty >>= finishStreaming send
+              Produce _ _ -> error "Shouldn't be producing right now"
+              Error -> error "Shouldn't be in an error state right now"
+              Done -> return ()
+        else sendResponse res
       ResponseRaw {} -> sendResponse res
   where
     (mEncs, reqWithoutBrotliAcceptEnc) =
@@ -106,3 +204,39 @@ brotli app req sendResponse = do
       case mEncs of
         Nothing -> (False, [])
         Just ebs -> canUseReqEncoding $ splitCommas ebs
+    finishStreaming send c =
+      case c of
+        Consume _ -> error "Shouldn't be consuming right now"
+        Produce b next ->
+          send (fromByteString b) >> next >>= finishStreaming send
+        Error -> error "Encountered error while finishing stream"
+        Done -> return ()
+    prefixTree = Trie.fromList $ zip (brotliMimePrefixes settings) (repeat ())
+
+-- TODO add to middleware
+fixHeaders :: [Header] -> [Header]
+fixHeaders = filter (\(k, _) -> k /= hContentLength)
+
+compressSend :: (Builder -> IO ()) -> IO () -> IORef (Compressor, Bool) -> Builder -> IO ()
+compressSend innerSend innerFlush st bs = do
+  let steps = L.toChunks $ toLazyByteString bs
+  mapM_ (step st) steps
+  where
+    step st bs = do
+      -- TODO this needs to produce until a consume is hit
+      (c, shouldFlush) <- readIORef st
+      case c of
+        Produce b n -> do
+          innerSend $ fromByteString b
+          when shouldFlush $ innerFlush
+          r <- n
+          writeIORef st (r, False)
+        Consume consumer -> do
+          r <- consumer bs
+          writeIORef st (r, shouldFlush)
+        Error -> error "Streaming send of response body failed in brotli compression phase"
+        Done -> error "Should not be done compressing while still sending data. Did you send an empty bytestring?"
+
+
+compressFlush :: IORef (Compressor, Bool) -> IO ()
+compressFlush ref = modifyIORef ref $ \(c, b) -> (c, True)
--- wai-middleware-brotli/test/Spec.hs
+++ wai-middleware-brotli/test/Spec.hs
@@ -2,18 +2,26 @@
 import Network.HTTP.Types
 import Codec.Compression.Brotli (decompress, compress)
 import Control.Concurrent.MVar
+import qualified Data.ByteString.Char8 as B
 import qualified Data.ByteString.Lazy as L
 import qualified Data.ByteString.Builder as Builder
+import Data.IORef
+import Data.Monoid
 import Network.Wai
-import Network.Wai.Handler.Warp
 import Network.Wai.Middleware.Brotli
 import Network.Wai.Internal
+import System.IO
 
 main :: IO ()
 main = do
+  hSetBuffering stdout LineBuffering
+  hSetBuffering stderr LineBuffering
+  putStrLn ""
+
   bod <- L.readFile "/usr/share/dict/words" -- "Hello World 000000000000000000000000000000"
   -- app :: Application
-  let app req respond = respond $ responseLBS status200 [] bod
+  putStr "Encoding text: "
+  let app req respond = respond $ responseLBS status200 [("Content-Type", "text/whatever")] bod
   var <- newEmptyMVar
   (brotli app)
     (defaultRequest {requestHeaders = [("Accept-Encoding", "br")]})
@@ -23,21 +31,25 @@ main = do
   resp <- takeMVar var
   case resp of
     ResponseBuilder stats hs b -> do
-      print hs
       print (decompress (Builder.toLazyByteString b) == bod)
     _ -> error "Not what we expected"
+  putStrLn ""
 
+  putStr "Not encoding if brotli not in Accept-Encoding header: "
   var <- newEmptyMVar
   (brotli app)
-    (defaultRequest {requestHeaders = [("Accept-Encoding", "gzip")]})
+    (defaultRequest {requestHeaders = [("Accept-Encoding", "gzip"), ("Content-Type", "text/whatever")]})
     (\resp -> do
        putMVar var resp
        return ResponseReceived)
   resp <- takeMVar var
   case resp of
-    ResponseBuilder stats hs b -> print (Builder.toLazyByteString b == bod)
+    ResponseBuilder stats hs b -> do
+      print (Builder.toLazyByteString b == bod)
     _ -> error "Not what we expected"
+  putStrLn ""
 
+  putStr "Decode request body if it's encoded: "
   var <- newEmptyMVar
   let reqWithBody =
         defaultRequest
@@ -58,6 +70,74 @@ main = do
     ResponseBuilder stats hs b ->
       print (Builder.toLazyByteString b == "Hello World")
     _ -> error "Not what we expected"
+  putStrLn ""
+
+  putStr "Streaming response body encoding: "
+  var <- newEmptyMVar
+  (brotli $ \req respond -> do
+      respond $ responseStream status200 [("Content-Type", "text/whatever")] $ \send flush -> do
+        send "Hello world\n"
+        flush
+        send "Goodbye world"
+    )
+    (defaultRequest {requestHeaders = [("Accept-Encoding", "br")]})
+    (\resp -> do
+       putMVar var resp
+       return ResponseReceived)
+  resp <- takeMVar var
+  r <- newIORef mempty
+  case resp of
+    ResponseStream stats hs f -> do
+      f (\b -> modifyIORef r (<> b)) (return ())
+      b <- readIORef r
+      print (decompress (Builder.toLazyByteString b) == "Hello world\nGoodbye world")
+    _ -> error "Not what we expected"
+  putStrLn ""
+
+  putStr "Don't encode non-textual content: "
+  let app req respond = respond $ responseLBS status200 [("Content-Type", "application/binary")] bod
+  var <- newEmptyMVar
+  (brotli app)
+    (defaultRequest {requestHeaders = [("Accept-Encoding", "br")]})
+    (\resp -> do
+       putMVar var resp
+       return ResponseReceived)
+  resp <- takeMVar var
+  case resp of
+    ResponseBuilder stats hs b -> do
+      print (Builder.toLazyByteString b == bod)
+    _ -> error "Not what we expected"
+  putStrLn ""
+
+  putStr "Don't encode if content length is too low: "
+  let app req respond = respond $ responseLBS status200 [("Content-Type", "application/json"), ("Content-Length", "2")] "{}"
+  var <- newEmptyMVar
+  (brotli app)
+    (defaultRequest {requestHeaders = [("Accept-Encoding", "br")]})
+    (\resp -> do
+       putMVar var resp
+       return ResponseReceived)
+  resp <- takeMVar var
+  case resp of
+    ResponseBuilder stats hs b -> do
+      print (Builder.toLazyByteString b == "{}")
+    _ -> error "Not what we expected"
+  putStrLn ""
+
+  putStr "Do encode if content length is long enough: "
+  let app req respond = respond $ responseLBS status200 [("Content-Type", "text/whatever"), ("Content-Length", B.pack $ show $ L.length bod)] bod
+  var <- newEmptyMVar
+  (brotli app)
+    (defaultRequest {requestHeaders = [("Accept-Encoding", "br")]})
+    (\resp -> do
+       putMVar var resp
+       return ResponseReceived)
+  resp <- takeMVar var
+  case resp of
+    ResponseBuilder stats hs b -> do
+      print (decompress (Builder.toLazyByteString b) == bod)
+    _ -> error "Not what we expected"
+  putStrLn ""
 
 -- curl -H "Accept-Encoding: br" http://leviathan.local:9450 | bro --decompress
 -- Should return Hello World etc.
